3bda92b8c56ca29cea21f3ca4a1beef8b821b962
unknown <unknown>
2025-05-14 17:11:15 +0530

    test

---
+++ Action.c
@@ -0,0 +1,24 @@
+Action()
+{
+	
+		lr_start_transaction("DPSOnly_TR01_Launch_Page");	
+	
+	web_url("{var_Environment}", 
+		"URL=https://blazedemo.com/", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=https://blazedemo.com/", 
+		"Snapshot=t3.inf", 
+		"Mode=HTML", 
+		/*EXTRARES, 
+		"Url={var_Protocol}://rep.checkpoint.com/rep-auth/service/v1.0/request", "Referer=", ENDITEM, */
+		LAST);
+
+
+
+	lr_end_transaction("DPSOnly_TR01_Launch_Page",LR_AUTO);
+	
+	
+	return 0;
+}
